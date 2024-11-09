a6c9899318ceb2a9cee6954c27b9b7595c971111
zkrajewska <nap29bal@gmail.com>
2024-11-10 00:58:56 +0300

    init: start artice fe

--- Other/Frontend-RSP/datascript.js
+++ Other/Frontend-RSP/datascript.js
@@ -1,5 +1,4 @@
-
-const apiUrl = 'https://backend-rsp/articles?status=1';
+const apiUrl = '../Backend-RSP/articles?status=1';
 
 fetch(apiUrl)
     .then(response => response.json())
@@ -10,7 +9,7 @@ fetch(apiUrl)
             const cardHTML = `
                 <div class="col">
                     <div class="card" style="width: 100%; background-color: #333; border: none;">
-                        <img class="card-img-top" src="../../Graphics/pics/cell-and-molecular-biology.png" alt="${article.Name}" style="width: 100%; height: 200px; object-fit: cover;">
+                        <img class="card-img-top" src="../Graphics/pics/cell-and-molecular-biology.png" alt="${article.Name}" style="width: 100%; height: 200px; object-fit: cover;">
                         <div class="card-body">
                             <h4 class="card-title" style="color: #F5F5F5;">${article.Name}</h4>
                             <p class="card-text" style="color: #F5F5F5;">${article.SubmissionDate}</p>
@@ -28,15 +27,15 @@ fetch(apiUrl)
     })
     .catch(error => console.error('Error fetching data:', error));
 
-const carouselApiUrl = 'https://backend-rsp/articles';
+const carouselApiUrl = '../Backend-RSP/articles';
 
 const articleIds = [1, 2, 3]; // Example IDs for articles
 
 // Fetch and populate Articles Data
 Promise.all(articleIds.map(id => 
-    fetch(`${carouselApiUrl}/${id}/`) // Fetch article by ID
+    fetch(${carouselApiUrl}/${id}/) // Fetch article by ID
     .then(response => response.json())
-    .catch(error => console.error(`Error fetching article ${id}:`, error))
+    .catch(error => console.error(Error fetching article ${id}:, error))
 )).then(data => {
     const carouselIndicators = document.getElementById('carousel-indicators');
     const carouselInner = document.getElementById('carousel-inner');
@@ -47,7 +46,7 @@ Promise.all(articleIds.map(id =>
         indicator.type = 'button';
         indicator.setAttribute('data-bs-target', '#demo');
         indicator.setAttribute('data-bs-slide-to', index);
-        indicator.setAttribute('aria-label', `Slide ${index + 1}`);
+        indicator.setAttribute('aria-label', Slide ${index + 1});
         if (index === 0) {
             indicator.classList.add('active');
             indicator.setAttribute('aria-current', 'true');
@@ -62,7 +61,7 @@ Promise.all(articleIds.map(id =>
         }
 
         const image = document.createElement('img');
-        image.src = "../../Graphics/pics/cell-and-molecular-biology.png";
+        image.src = "../Graphics/pics/cell-and-molecular-biology.png";
         image.alt = article.author_name;
         image.classList.add('d-block', 'w-100');
 
